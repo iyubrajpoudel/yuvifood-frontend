88bed1108b64ca6a6095a78a706440a158462a35
Yubraj <imyubraz@gmail.com>
2023-05-26 23:18:28 +0545

    Clutter cleared, global css rule added in globals.css.

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
