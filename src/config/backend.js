8604a483c9ca2d3f8e508095e55e5624966d8223
Yubraj <iyubrajpoudel@gmail.com>
2023-05-27 21:24:35 +0545

    Few configurations before deploying our app...

--- src/config/backend.js
+++ src/config/backend.js
@@ -1,2 +1,3 @@
+// export const BASE_URL = process.env.BASE_URL || `http://localhost:8000`
 
-export const BASE_URL = `http://localhost:8000`
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || `http://localhost:8000`
